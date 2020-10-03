d5cf8105eff1e20c8e206e34c291162872478794
Sonali <sonali.kad@wwindia.com>
2020-10-03 15:28:23 +0530

    Updates

---
+++ precache-manifest.26ef8826b174e31f3b8a1fad584efded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b4a71c590cece083c003db16ac39e3",
+    "url": "/react-crud-app/index.html"
+  },
+  {
+    "revision": "97bdd71a58992d6c4db5",
+    "url": "/react-crud-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0db53702d00f4a02e559",
+    "url": "/react-crud-app/static/css/main.eaefd058.chunk.css"
+  },
+  {
+    "revision": "97bdd71a58992d6c4db5",
+    "url": "/react-crud-app/static/js/2.985e90db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crud-app/static/js/2.985e90db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db53702d00f4a02e559",
+    "url": "/react-crud-app/static/js/main.44b444cc.chunk.js"
+  },
+  {
+    "revision": "a0b934650bb2911db55a",
+    "url": "/react-crud-app/static/js/runtime-main.9bd8629e.js"
+  }
+]);
